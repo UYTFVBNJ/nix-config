b07c42fa6f3d5fe77aecc90b9b8becd5e5711c82
Julian Gao <juytfvbng@gamil.com>
2025-10-31 17:51:30 +0800

    fix: change GITHUB_TOKEN from path to token

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -8,5 +8,14 @@
     enable = true;
 
     # ... Other options ...
+    extraConfig = {
+      # this is the same helper you tried to run by hand
+      credential.helper = ''
+        !f() {
+          echo "username=x-access-token"
+          echo "password=$GH_TOKEN"
+        }; f
+      '';
+    };
   };
 }
